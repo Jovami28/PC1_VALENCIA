c30cc65a87c73e753bac9f24ed5ef9344c0d75c5
Jovami28 <118119052+Jovami28@users.noreply.github.com>
2023-09-09 20:33:57 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Función para redireccionar a la página de Wikipedia del equipo
+function redirectToWikipedia4(equipo) {
+  // Reemplaza 'espacios' con '_' en el nombre del equipo
+  equipo = equipo.replace(/ /g, "_");
+
+  // Redirecciona a la página de Wikipedia del equipo
+  window.location.href = `https://es.wikipedia.org/wiki/Sport_Boys_Association`;
+}
+function redirectToWikipedia3(equipo) {
+  // Reemplaza 'espacios' con '_' en el nombre del equipo
+  equipo = equipo.replace(/ /g, "_");
+
+  // Redirecciona a la página de Wikipedia del equipo
+  window.location.href = `https://es.wikipedia.org/wiki/Club_Universitario_de_Deportes`;
+}
+function redirectToWikipedia2(equipo) {
+  // Reemplaza 'espacios' con '_' en el nombre del equipo
+  equipo = equipo.replace(/ /g, "_");
+
+  // Redirecciona a la página de Wikipedia del equipo
+  window.location.href = `https://es.wikipedia.org/wiki/Club_Alianza_Lima`;
+}
+function redirectToWikipedia1(equipo) {
+  // Reemplaza 'espacios' con '_' en el nombre del equipo
+  equipo = equipo.replace(/ /g, "_");
+
+  // Redirecciona a la página de Wikipedia del equipo
+  window.location.href = `https://es.wikipedia.org/wiki/Club_Sporting_Cristal`;
+}
